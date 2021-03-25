7b634b192c879114cad0f6661156dc2c15b44220
f.sousa <ng.dev.sousa@outlook.com>
2021-03-24 22:00:04 -0300

    Add Stripe to retrieve product price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
-import styles from "./home.module.scss"
+import { stripe } from "../services/stripe";
+import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -16,13 +24,26 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90/month</span>
-            <SubscribeButton/>
+            <span>for {product.amount}/month</span>
+            <SubscribeButton priceId={product.priceId} />
           </p>
         </section>
         <img src="/images/avatar.svg" alt="Girl coding" />
-        
       </main>
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IYhBbEhm5bOwWwGxvjKZ1mA");
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD"
+    }).format(price.unit_amount / 100)
+  };
+  return {
+    props: { product }
+  };
+};
