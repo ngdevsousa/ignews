0efd9a00c6e2c04221868930c417925babbc68d1
f.sousa <ng.dev.sousa@outlook.com>
2021-03-25 21:42:31 -0300

    Add login with Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,27 @@
 import { FaGithub } from "react-icons/fa";
-import { FiX } from "react-icons/fi"
+import { FiX } from "react-icons/fi";
 import styles from "./styles.module.scss";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export function SignInButton() {
-  const isLoggedIn = true;
-  return isLoggedIn ? (
+  const [session] = useSession();
+
+  return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
-      Sign in with Github
-      <FiX color="#737380" className={styles.closeIcon}/>
+      {session.user.name}
+      <FiX
+        color="#737380"
+        className={styles.closeIcon}
+        onClick={() => signOut()}
+      />
     </button>
   ) : (
-    <button type="button" className={styles.signInButton}>
+    <button
+      type="button"
+      className={styles.signInButton}
+      onClick={() => signIn("github")}
+    >
       <FaGithub color="#eba417" />
       Sign in with Github
     </button>
