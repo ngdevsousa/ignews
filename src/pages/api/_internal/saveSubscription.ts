76c27ab00f1b6521bfcab5e230a936ff6db43284
f.sousa <ng.dev.sousa@outlook.com>
2021-03-27 13:38:23 -0300

    Add handler for subscription update

--- src/pages/api/_internal/saveSubscription.ts
+++ src/pages/api/_internal/saveSubscription.ts
@@ -1,10 +1,11 @@
-import { fauna } from "../../../services/fauna";
 import { query as q } from "faunadb";
+import { fauna } from "../../../services/fauna";
 import { stripe } from "../../../services/stripe";
 
 export async function saveSubscription(
   subscriptionId: string,
-  customerId: string
+  customerId: string,
+  insertNewRecord = false
 ) {
   const userRef = await fauna.query(
     q.Select(
@@ -26,10 +27,31 @@ export async function saveSubscription(
     priceId: subscription.items.data[0].price.id
   }
 
-  await fauna.query(
-    q.Create(
-      q.Collection("subscriptions"),
-      { data: { subscriptionData}}
-    )
-  )
+  try {
+    if(insertNewRecord) {
+      await fauna.query(
+        q.Create(
+          q.Collection("subscriptions"),
+          { data: subscriptionData }
+        )
+      )
+    } else {
+      await fauna.query(
+        q.Replace(
+          q.Select(
+            "ref",
+            q.Get(
+              q.Match(
+                q.Index("subscription_by_id"),
+                subscriptionId
+              )
+            )
+          ),
+          { data: subscriptionData }
+        )
+      )
+    }
+  } catch (error) {
+    console.log(error)
+  }
 }
--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -20,7 +20,11 @@ export const config = {
   }
 };
 
-const relevantEvents = new Set(["checkout.session.completed"]);
+const relevantEvents = new Set([
+  "checkout.session.completed",
+  "customer.subscription.updated",
+  "customer.subscription.deleted"
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
@@ -41,18 +45,30 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(event.type)) {
       try {
         switch (event.type) {
+          case "customer.subscription.updated":
+          case "customer.subscription.deleted":
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString()
+            );
+            break;
           case "checkout.session.completed":
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
+
             await saveSubscription(
               checkoutSession.subscription.toString(),
-              checkoutSession.customer.toString()
+              checkoutSession.customer.toString(),
+              true
             );
             break;
           default:
             throw new Error("Handler Not Implemented");
         }
       } catch (error) {
+        console.log(error)
         return res.json({ error: "Not Implemented" });
       }
     }
