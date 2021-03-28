780c8ff97fcf0d6df8ced6e0c67019572d41a633
f.sousa <ng.dev.sousa@outlook.com>
2021-03-28 11:30:35 -0300

    Add Post Component with styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,6 @@ import { SignInButton } from "../SignInButton";
 import styles from "./styles.module.scss";
 
 export function Header() {
-  
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
